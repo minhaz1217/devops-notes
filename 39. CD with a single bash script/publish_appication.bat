4d0d8430392df9988e46da1246a2a5fc262bf232
Minhaz <minhaz1217@gmail.com>
2022-06-26 22:00:17 +0600

    added necessary comments

--- 39. CD with a single bash script/publish_appication.bat
+++ 39. CD with a single bash script/publish_appication.bat
@@ -9,49 +9,52 @@ set project_relative_path_to_drive=\MyComputer\website\c#\simple dotnet app
 set project_to_build_relative_path=\simple dotnet app
 set output_relative_path=/bin/publish
 
-
+@REM Making git directory from the given information (to change the git branch)
 set git_directory=%git_directory_drive%%project_relative_path_to_drive%
 
-
+@REM Making the needed WSL paths (for uploading)
 set project_relative_path_to_drive_wsl=%project_relative_path_to_drive:\=/%
 set project_to_build_relative_path_wsl=%project_to_build_relative_path:\=/%
-set relative_output_directory_wsl=%relative_output_directory:\=/%
+set relative_project_output_path_wsl=%relative_project_output_path:\=/%
 set output_relative_path_wsl=%output_relative_path:\=/%
 set published_folder_wsl="%git_directory_drive_wsl%%project_relative_path_to_drive_wsl%%project_to_build_relative_path_wsl%%output_relative_path_wsl%"
 
+@REM Making the remote machine key path from given windows path.
 set private_key_drive_wsl=/mnt/c
 set private_key_relative_location_to_drive=\Users\HA HA\amazon_key
 set private_key_relative_location_to_drive_wsl=%private_key_relative_location_to_drive:\=/%
 set private_key_wsl_location=%private_key_drive_wsl%%private_key_relative_location_to_drive_wsl%
 
+@REM Remote machine information
 set remote_host=15.207.249.72
 set remote_user=ubuntu
 set remote_location="~/project"
 
+@REM Information needed for the app to run and to restart.
 set app_dll_file=simple dotnet app.dll
 set app_port=5000
 
-
+@REM Building path for building and outputting the project.
 set project_to_build=%git_directory%%project_to_build_relative_path%
-set output_directory=%git_directory%%project_to_build_relative_path%%output_relative_path_wsl%
+set project_output_path=%git_directory%%project_to_build_relative_path%%output_relative_path_wsl%
 
 echo %project_to_build_path%
 @REM echo %git_directory% %project_to_build_path%
-echo %output_directory%
+echo %project_output_path%
 
 @REM At first stash all current changes, if needed.
 @REM git -C "%git_directory%" stash
 
 @REM @REM Change git branch
 git -C "%git_directory%" checkout %git_branch%
-
 git -C "%git_directory%" pull origin %git_branch%
 
 @REM @REM Build the project
 echo "Building the project: %project_to_build%"
-@REM dotnet publish -c Release --no-self-contained -r linux-x64 -o "%output_directory%" "%project_to_build%"
+echo "Outputting the project to: %project_output_path%"
+dotnet publish -c Release --no-self-contained -r linux-x64 -o "%project_output_path%" "%project_to_build%"
 
-@REM dotnet publish -c Release -f netcoreapp3.1 --no-self-contained -r linux-x64 -o %output_directory% %project_to_build%
+@REM dotnet publish -c Release -f netcoreapp3.1 --no-self-contained -r linux-x64 -o %project_output_path% %project_to_build%
 
 
 @REM @REM Copy additional files
@@ -65,11 +68,13 @@ wsl.exe -d Ubuntu -u root sudo rsync -avzh -e "ssh -i \"%private_key_wsl_locatio
 
 @REM ssh -i "/mnt/c/Users/HA HA/amazon_key" ubuntu@65.0.181.197:~/project
 @REM chmod 400 amazon_key -f
-@REM Stop the app 
+
+
+@REM Stopping the app 
 wsl.exe -d Ubuntu -u root sudo ssh -i "%private_key_wsl_location%" %remote_user%@%remote_host% "sudo lsof -ti:%app_port% | sudo xargs kill -9"
 
 
-@REM Start the app
+@REM Starting the app
 wsl.exe -d Ubuntu -u root sudo ssh -i "%private_key_wsl_location%" %remote_user%@%remote_host% "cd %remote_location%; /snap/dotnet-sdk/current/dotnet \"%app_dll_file%\""
 
 @REM If added via apt install
